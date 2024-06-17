2de4ae8d400cc2f1f9169f75fd14f6cb59ed7a89
FKHAN9644 <frk9644@gmail.com>
2024-06-17 03:30:55 -0400

    fixing 404

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch, useHistory } from 'react-router-dom';
 import Home from './Components/Home';
 import About from './Components/About';
 import Projects from './Components/Projects';
@@ -7,10 +7,24 @@ import Resume from './Components/Resume';
 import Contact from './Components/Contact';
 import MenuBar from './Components/MenuBar';
 
+function RedirectHandler() {
+  const history = useHistory();
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const redirectPath = urlParams.get('redirect');
+    if (redirectPath) {
+      history.replace(redirectPath);
+    }
+  }, [history]);
+
+  return null;
+}
 function App() {
   return (
     <Router>
       <div>
+      <RedirectHandler />
         <MenuBar />
         <Routes>
           <Route path="/" element={<Home />} />
